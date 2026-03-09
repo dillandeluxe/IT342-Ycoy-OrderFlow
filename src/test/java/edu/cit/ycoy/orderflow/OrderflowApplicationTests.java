073592ceabe551b9cda240d44942a5c1c4da5d72
LAB24_USER ON WS26 <L24X07W26@LabHCI.net>
2026-03-09 08:21:27 +0800

    Initial commit: Spring Boot boilerplate setup

---
+++ src/test/java/edu/cit/ycoy/orderflow/OrderflowApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.ycoy.orderflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
